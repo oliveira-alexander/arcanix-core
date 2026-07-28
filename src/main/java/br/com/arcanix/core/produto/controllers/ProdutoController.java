23433e98b98522c8fae77b0f3addd3e3960889e6
Alexander <oliveir4.4lexander@gmail.com>
2026-07-28 14:46:38 -0300

    Implementado cadastro de produto

--- src/main/java/br/com/arcanix/core/produto/controllers/ProdutoController.java
+++ src/main/java/br/com/arcanix/core/produto/controllers/ProdutoController.java
@@ -1,9 +1,46 @@
 package br.com.arcanix.core.produto.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.arcanix.core.produto.dtos.produto.CreateProdutoDTO;
+import br.com.arcanix.core.produto.dtos.produto.ResponseProdutoDTO;
+import br.com.arcanix.core.produto.dtos.produto.UpdateProdutoDTO;
+import br.com.arcanix.core.produto.services.ProdutoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/produtos")
+@RequiredArgsConstructor
 public class ProdutoController {
+
+    private final ProdutoService service;
+
+    @PostMapping
+    public ResponseEntity<ResponseProdutoDTO> create(@RequestBody CreateProdutoDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.create(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResponseProdutoDTO>> findAll(){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(service.findAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseProdutoDTO> update(@PathVariable Long id,
+    @RequestBody UpdateProdutoDTO dto){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(service.update(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/br/com/arcanix/core/produto/services/impl/ProdutoServiceImpl.java
+++ src/main/java/br/com/arcanix/core/produto/services/impl/ProdutoServiceImpl.java
@@ -1,8 +1,101 @@
 package br.com.arcanix.core.produto.services.impl;
 
+import br.com.arcanix.core.pessoa.entities.Pessoa;
+import br.com.arcanix.core.pessoa.repository.PessoaRepository;
+import br.com.arcanix.core.produto.dtos.categoriaProduto.ResponseCategoriaProdutoDTO;
+import br.com.arcanix.core.produto.dtos.produto.CreateProdutoDTO;
+import br.com.arcanix.core.produto.dtos.produto.ResponseFornecedorDTO;
+import br.com.arcanix.core.produto.dtos.produto.ResponseProdutoDTO;
+import br.com.arcanix.core.produto.dtos.produto.UpdateProdutoDTO;
+import br.com.arcanix.core.produto.entities.CategoriaProduto;
+import br.com.arcanix.core.produto.entities.Produto;
+import br.com.arcanix.core.produto.mapper.ProdutoMapper;
+import br.com.arcanix.core.produto.repository.CategoriaProdutoRepository;
+import br.com.arcanix.core.produto.repository.ProdutoRepository;
 import br.com.arcanix.core.produto.services.ProdutoService;
+import br.com.arcanix.core.shared.exceptions.categoriaProduto.CategoriaProdutoNotFoundException;
+import br.com.arcanix.core.shared.exceptions.pessoa.PessoaNotFoundException;
+import br.com.arcanix.core.shared.exceptions.produto.ProdutoNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class ProdutoServiceImpl implements ProdutoService {
+
+    private final ProdutoMapper mapper;
+    private final ProdutoRepository produtoRepository;
+    private final CategoriaProdutoRepository categoriaRepository;
+    private final PessoaRepository pessoaRepository;
+
+
+    @Override
+    public ResponseProdutoDTO create(CreateProdutoDTO dto) {
+        Produto entity = mapper.createDtoToEntity(dto);
+
+        CategoriaProduto categoria = categoriaRepository.findById(dto.categoriaId())
+                .orElseThrow(() -> new CategoriaProdutoNotFoundException("Categoria de Produto não encontrada!"));
+
+        Pessoa fornecedor = pessoaRepository.findById(dto.fornecedorId())
+                        .orElseThrow(() -> new PessoaNotFoundException("Fornecedor não encontrado!"));
+
+        entity.setCategoria(categoria);
+        entity.setFornecedor(fornecedor);
+
+        produtoRepository.save(entity);
+
+        return mapper.entityToResponseDto(
+                mapper.categoriaProdutoToResponse(categoria),
+                mapper.pessoaToFornecedorResponse(fornecedor),
+                entity);
+    }
+
+    @Override
+    public ResponseProdutoDTO update(Long id, UpdateProdutoDTO dto) {
+        Produto entity = produtoRepository.findById(id)
+                .orElseThrow(() -> new ProdutoNotFoundException("Produto não encontrado!"));
+
+        mapper.updateDtoToEntity(dto, entity);
+
+        CategoriaProduto categoria = categoriaRepository.findById(dto.categoriaId()).get();
+        Pessoa fornecedor = pessoaRepository.findById(dto.fornecedorId()).get();
+
+        entity.setCategoria(categoria);
+        entity.setFornecedor(fornecedor);
+
+        produtoRepository.save(entity);
+
+        return mapper.entityToResponseDto(
+                mapper.categoriaProdutoToResponse(categoria),
+                mapper.pessoaToFornecedorResponse(fornecedor),
+                entity);
+    }
+
+    @Override
+    public List<ResponseProdutoDTO> findAll() {
+        return produtoRepository.findAll()
+                .stream()
+                .map(p -> {
+
+                    ResponseCategoriaProdutoDTO responseCategoria = mapper.categoriaProdutoToResponse(p.getCategoria());
+                    ResponseFornecedorDTO fornecedor = mapper.pessoaToFornecedorResponse(p.getFornecedor());
+
+                    return mapper.entityToResponseDto(responseCategoria, fornecedor, p);
+                })
+                .toList();
+
+
+    }
+
+    @Override
+    public void delete(Long id) {
+        Produto entity = produtoRepository.findById(id)
+                .orElseThrow(() -> new ProdutoNotFoundException("Produto não encontrado!"));
+
+        produtoRepository.delete(entity);
+    }
 }
