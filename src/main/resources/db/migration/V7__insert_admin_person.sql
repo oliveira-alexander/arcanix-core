9874e96c6ab5cff5e497bfe7be6a2e9db52a4a91
Alexander <oliveir4.4lexander@gmail.com>
2026-08-14 10:22:26 -0300

    Adicionado insert de Pessoa Administrador

---
+++ src/main/resources/db/migration/V7__insert_admin_person.sql
@@ -0,0 +1,9 @@
+INSERT INTO PESSOAS VALUES (
+                    -1,
+                    'ADMINISTRADOR',
+                    'ADMINISTRADOR',
+                    '123.456.789-10',
+                    NULL,
+                    'PESSOA_FISICA',
+                    'CLIENTE'
+);
