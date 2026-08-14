491fdd21af6a6b2def4c0cb6c09398424436fdfd
Alexander <oliveir4.4lexander@gmail.com>
2026-08-14 10:41:39 -0300

    Adicionado SecurityConfig e PasswordEncoder

---
+++ src/main/java/br/com/arcanix/core/security/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package br.com.arcanix.core.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http)
+        throws Exception{
+
+        http.cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**",
+                                "/v3/api-docs/**")
+                        .permitAll()
+
+                        .anyRequest()
+                        .authenticated()
+                );
+
+        return http.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
